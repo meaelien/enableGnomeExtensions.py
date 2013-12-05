b419fba7b01740bf5901dc961568b39e7e8ad790
Loric Brevet <loric.brevet@gmail.com>
2013-12-05 17:48:16 +0100

    Addition of the script file

---
+++ enableGnomeExtensions.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from os import listdir
+from os import popen
+from os.path import isdir, join, expanduser
+import time
+
+pre_installed_ext_folder = "/usr/share/gnome-shell/extensions"
+my_ext_folder = "{}/.local/share/gnome-shell/extensions".format(expanduser("~"))
+
+######## GET EXTENSIONS ###########
+
+def getAllExtensions(): 
+	extensions = [d for d in listdir(pre_installed_ext_folder) if isdir(join(pre_installed_ext_folder,d))] # Get pre installed extensions
+	extensions = extensions + [d for d in listdir(my_ext_folder) if isdir(join(my_ext_folder,d))] # Add my extensions
+	return extensions
+
+def getEnabledExtensions():
+	extensions_string = popen("gsettings get org.gnome.shell enabled-extensions").read()
+	extensions = [extension.strip("'") for extension in extensions_string.strip().strip('[').strip(']').split(',')]
+	return extensions
+
+def getDisabledExtensions():
+	extensions = [extension for extension in getAllExtensions() if extension not in getEnabledExtensions()]
+	return extensions
+
+####### ENABLE/DISABLE EXTENSIONS ##########
+
+def enableAllExtensions():
+	popen("gsettings set org.gnome.shell enabled-extensions \"{}\"".format(getAllExtensions()))
+
+def enableOneExtension(extension):
+	popen("gnome-shell-extension-tool -e {}".format(extension))
+
+def disableAllExtensions():
+	popen("gsettings set org.gnome.shell enabled-extensions \"[]\"")
+
+def disableOneExtension(extension):
+	popen("gnome-shell-extension-tool -d {}".format(extension))
+
+####### OTHERS #########
+
+# Sometimes, it's necessary to restart gnome shell if not startup
+def restartGnomeShell():
+	popen("nohup gnome-shell --replace &")
+
+####### MAIN ##########
+
+if __name__ == '__main__':
+	time.sleep(2) # Just in case
+	enableAllExtensions()
